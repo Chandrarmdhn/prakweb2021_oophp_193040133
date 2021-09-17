ebd731884fb5a3f1da7d480c9a7c6bca1b37201f
unknown <e3000yoisantuy@gmail.com>
2021-09-17 16:51:05 +0700

    membuat file dengan nama coba.php yang berisi class Coba

--- coba.php
+++ coba.php
@@ -4,9 +4,18 @@
 
     //method
     public function b(){
-      
+
     }
   }
+
+  &a = new Coba();
+  &b = new Coba();
+  &c = new Coba();
+
+  var_dump($a);
+  var_dump($b);
+  var_dump($c);
+
   
 
 ?>
